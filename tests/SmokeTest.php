ef4482d31315cc40f6322d0b4d82e91d3e7f617b
Tobias Redmann <tobias.redmann@gmail.com>
2026-05-06 21:16:57 +0200

    chore: bootstrap project skeleton with PSR-4 autoload and PHPUnit

---
+++ tests/SmokeTest.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Composerd\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class SmokeTest extends TestCase
+{
+    public function testAutoloaderIsWired(): void
+    {
+        self::assertTrue(class_exists(\PHPUnit\Framework\TestCase::class));
+        self::assertSame('Composerd\\Tests', __NAMESPACE__);
+    }
+}
